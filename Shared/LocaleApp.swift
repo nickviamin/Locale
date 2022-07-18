fe7c313509ca6cb00906294e5e1a161dc8662281
nickviamin <61365510+nickviamin@users.noreply.github.com>
2022-07-18 16:23:17 -0400

    Initial Commit

---
+++ Shared/LocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleApp.swift
+//  Shared
+//
+//  Created by Nick Viamin on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
